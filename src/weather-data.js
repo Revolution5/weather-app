31009c88784742597ff41ad723ed4259133bf805
Revolution5 <damonboone99@gmail.com>
2022-05-18 15:32:53 -0700

    Change API request to HTTPS

--- src/weather-data.js
+++ src/weather-data.js
@@ -3,7 +3,7 @@
 export async function getWeatherData(city) {
     try {
         //convert city name to coordinates
-        const responseCoords = await fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=0a797fb7828e6df998b5f208074b34be", 
+        const responseCoords = await fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=0a797fb7828e6df998b5f208074b34be", 
         {mode: "cors"});
         const cityCoords = await responseCoords.json();
         let lat = cityCoords[0].lat;
